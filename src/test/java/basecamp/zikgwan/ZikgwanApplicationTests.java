b288b3ed41fdfcad1d33d98a4261333c271afba9
WARDKEY <tjrmsduq@naver.com>
2025-09-25 16:21:39 +0900

    프로젝트 초기 세팅

---
+++ src/test/java/basecamp/zikgwan/ZikgwanApplicationTests.java
@@ -0,0 +1,13 @@
+package basecamp.zikgwan;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZikgwanApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
