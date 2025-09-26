e1f131228fb671035df0e5c1ef5c3eb293dbb1f1
WARDKEY <tjrmsduq@naver.com>
2025-09-26 21:25:34 +0900

    feat(MatchSchedule): 경기 일정 테스트

--- src/main/java/basecamp/zikgwan/matchschedule/controller/MatchScheduleController.java
+++ src/main/java/basecamp/zikgwan/matchschedule/controller/MatchScheduleController.java
@@ -1,12 +1,27 @@
 package basecamp.zikgwan.matchschedule.controller;
 
+import basecamp.zikgwan.matchschedule.dto.KboRequestDto;
+import basecamp.zikgwan.matchschedule.dto.KboResponseDto;
 import basecamp.zikgwan.matchschedule.service.MatchScheduleService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequiredArgsConstructor
+@RequestMapping("/match")
 public class MatchScheduleController {
 
     private final MatchScheduleService matchScheduleService;
+
+    @PostMapping("/schedule")
+    public ResponseEntity<KboResponseDto> getSchedule(@RequestBody KboRequestDto kboRequestDto) {
+        KboResponseDto response = matchScheduleService.getSchedule(kboRequestDto);
+
+        return ResponseEntity.ok().body(response);
+    }
+
 }
--- src/main/java/basecamp/zikgwan/matchschedule/service/MatchScheduleService.java
+++ src/main/java/basecamp/zikgwan/matchschedule/service/MatchScheduleService.java
@@ -1,9 +1,15 @@
 package basecamp.zikgwan.matchschedule.service;
 
+import basecamp.zikgwan.matchschedule.dto.KboRequestDto;
+import basecamp.zikgwan.matchschedule.dto.KboResponseDto;
 import basecamp.zikgwan.matchschedule.repository.MatchScheduleRepository;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.client.reactive.ClientHttpRequest;
 import org.springframework.stereotype.Service;
+import org.springframework.web.reactive.function.BodyInserter;
+import org.springframework.web.reactive.function.client.WebClient;
+import reactor.core.publisher.Mono;
 
 @Slf4j
 @Service
@@ -11,4 +17,17 @@ import org.springframework.stereotype.Service;
 public class MatchScheduleService {
 
     private final MatchScheduleRepository matchScheduleRepository;
+    private final WebClient webClient;
+
+    public KboResponseDto getSchedule(KboRequestDto kboRequestDto) {
+
+        KboResponseDto kboResponseDto = webClient.post()
+                .uri("/")
+                .body(Mono.just(kboRequestDto), KboRequestDto.class)
+                .retrieve()
+                .bodyToMono(KboResponseDto.class)
+                .block();
+
+        return kboResponseDto;
+    }
 }
