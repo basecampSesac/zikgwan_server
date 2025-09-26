30f1a3d9deb0a52284ae8ae4da7c2b62bfea9ef2
WARDKEY <tjrmsduq@naver.com>
2025-09-26 17:40:41 +0900

    feat(Review): 평가 repository 세팅

---
+++ src/main/java/basecamp/zikgwan/review/repository/ReviewRepository.java
@@ -0,0 +1,9 @@
+package basecamp.zikgwan.review.repository;
+
+import basecamp.zikgwan.review.Review;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ReviewRepository extends JpaRepository<Review, Long> {
+}
