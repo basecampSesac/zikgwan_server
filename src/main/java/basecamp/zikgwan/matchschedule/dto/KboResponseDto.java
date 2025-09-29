5fc510ffb7264c795b431fca0a79569da0c65b50
WARDKEY <tjrmsduq@naver.com>
2025-09-29 21:23:38 +0900

    feat(MatchSchedule): 자정에 경기 일정 업데이트 및 경기 일정 조회 추가

--- src/main/java/basecamp/zikgwan/matchschedule/dto/KboResponseDto.java
+++ src/main/java/basecamp/zikgwan/matchschedule/dto/KboResponseDto.java
@@ -1,62 +1,16 @@
 package basecamp.zikgwan.matchschedule.dto;
 
-import java.util.List;
+import java.time.LocalDate;
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 
 @Data
 @Builder
+@AllArgsConstructor
 public class KboResponseDto {
-    private List<ScoreboardDto> scoreboard;
-    private List<DayDto> day;
-}
-
-@Data
-class ScoreboardDto {
-    private String idx;
-    private String team;
-    private int result;
-    private Object i_1;
-    private Object i_2;
-    private Object i_3;
-    private Object i_4;
-    private Object i_5;
-    private Object i_6;
-    private Object i_7;
-    private Object i_8;
-    private Object i_9;
-    private Object i_10;
-    private Object i_11;
-    private Object i_12;
-    private Object i_13;
-    private Object i_14;
-    private Object i_15;
-    private Object i_16;
-    private Object i_17;
-    private Object i_18;
-    private int r;
-    private int h;
-    private int e;
-    private int b;
-    private int year;
-    private int month;
-    private int day;
-    private String home;
-    private String away;
-    private int dbheader;
-    private String place;
-    private int audience;
-    private String starttime;
-    private String endtime;
-    private String gametime;
-}
-
-@Data
-class DayDto {
-    private String status;
-    private String date;
-    private String home;
-    private String away;
-    private int dbheader;
-    private String gameid;
+    private LocalDate date;    // 경기날짜
+    private String home;    // 홈팀 코드
+    private String away;    // 어웨이팀 코드
+    private String place;   // 경기장 정보
 }
--- src/main/java/basecamp/zikgwan/matchschedule/service/MatchScheduleService.java
+++ src/main/java/basecamp/zikgwan/matchschedule/service/MatchScheduleService.java
@@ -1,15 +1,26 @@
 package basecamp.zikgwan.matchschedule.service;
 
+import basecamp.zikgwan.matchschedule.MatchSchedule;
 import basecamp.zikgwan.matchschedule.dto.KboRequestDto;
 import basecamp.zikgwan.matchschedule.dto.KboResponseDto;
+import basecamp.zikgwan.matchschedule.mapper.TeamMapper;
 import basecamp.zikgwan.matchschedule.repository.MatchScheduleRepository;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+import java.util.NoSuchElementException;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.http.client.reactive.ClientHttpRequest;
+import org.jetbrains.annotations.NotNull;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.core.ParameterizedTypeReference;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
-import org.springframework.web.reactive.function.BodyInserter;
-import org.springframework.web.reactive.function.client.WebClient;
-import reactor.core.publisher.Mono;
+import org.springframework.web.client.RestTemplate;
 
 @Slf4j
 @Service
@@ -17,17 +28,137 @@ import reactor.core.publisher.Mono;
 public class MatchScheduleService {
 
     private final MatchScheduleRepository matchScheduleRepository;
-    private final WebClient webClient;
+    private final RestTemplate restTemplate;
 
-    public KboResponseDto getSchedule(KboRequestDto kboRequestDto) {
+    @Value("${kbo.data.url}")
+    private String url; // kbodate 요청용 url
 
-        KboResponseDto kboResponseDto = webClient.post()
-                .uri("/")
-                .body(Mono.just(kboRequestDto), KboRequestDto.class)
-                .retrieve()
-                .bodyToMono(KboResponseDto.class)
-                .block();
+    // 패턴 정의 (yyyyMMdd)
+    private final DateTimeFormatter FORMATTER = DateTimeFormatter.ofPattern("yyyyMMdd");
 
-        return kboResponseDto;
+    // 오늘 포함 ±10일 간의 경기 일정을 저장
+    public List<KboResponseDto> saveScheduleRange(LocalDate today) {
+        List<KboResponseDto> dtos = new ArrayList<>();
+
+        for (int i = -10; i <= 10; i++) {
+            LocalDate targetDate = today.plusDays(i);
+
+            List<KboResponseDto> responseDtos = saveSchedule(
+                    targetDate.getYear(),
+                    targetDate.getMonthValue(),
+                    targetDate.getDayOfMonth()
+            );
+
+            dtos.addAll(responseDtos);
+        }
+
+        return dtos;
+    }
+
+    // DB에 경기 일정 저장
+    private List<KboResponseDto> saveSchedule(int year, int month, int day) {
+
+        KboRequestDto date = KboRequestDto.builder()
+                .year(year)
+                .month(month)
+                .day(day)
+                .build();
+
+        ResponseEntity<Map<String, Object>> responseEntity =
+                restTemplate.exchange(
+                        url,
+                        HttpMethod.POST,
+                        new HttpEntity<>(date),
+                        new ParameterizedTypeReference<>() {
+                        }
+                );
+
+        Map<String, Object> response = responseEntity.getBody();
+
+        // "day" 키의 데이터만 추출
+        List<Map<String, Object>> dayList = (List<Map<String, Object>>) response.get("day");
+
+        List<KboResponseDto> responseDtos = convertToDto(dayList, response);
+
+        saveDB(responseDtos);
+
+        return responseDtos;
+    }
+
+    // 경기 일정 조회
+    public List<KboResponseDto> getSchedule(KboRequestDto kboRequestDto) {
+        LocalDate matchDate = LocalDate.of(kboRequestDto.getYear(), kboRequestDto.getMonth(), kboRequestDto.getDay());
+
+        List<MatchSchedule> matchSchedules = matchScheduleRepository.findAllByMatchDate(matchDate);
+
+        if (matchSchedules.isEmpty()) {
+            throw new NoSuchElementException("경기 일정을 찾을 수 없습니다.");
+        }
+
+        return matchSchedules.stream()
+                .map(m -> {
+                    return KboResponseDto.builder()
+                            .date(m.getMatchDate())
+                            .home(m.getHomeTeam())
+                            .away(m.getAwayTeam())
+                            .place(m.getStadium())
+                            .build();
+                }).toList();
+    }
+
+    @NotNull
+    private List<KboResponseDto> convertToDto(List<Map<String, Object>> dayList, Map<String, Object> response) {
+        return dayList.stream()
+                .map(d -> {
+                    // 문자열 날짜 → LocalDate 변환
+                    LocalDate localDate = LocalDate.parse((String) d.get("date"), FORMATTER);
+
+                    return KboResponseDto.builder()
+                            .date(localDate)
+                            .home((String) d.get("home"))
+                            .away((String) d.get("away"))
+                            .place(findPlace(response, d.get("home"), d.get("away"), localDate.getYear()))
+                            .build();
+                })
+                .toList();
+    }
+
+    private void saveDB(List<KboResponseDto> responseDtos) {
+        List<MatchSchedule> matchSchedules = responseDtos.stream()
+                .map(r -> {
+                    return matchScheduleRepository.findByMatchDateAndHomeTeamAndAwayTeam(
+                            r.getDate(), r.getHome(), r.getAway()
+                    ).map(m -> {
+                        // 이미 있으면 업데이트
+                        m.updateStadium((r.getPlace() == null || r.getPlace().isBlank()) ? "미정" : r.getPlace());
+                        return m;
+                    }).orElseGet(() -> {
+                        // 없으면 새로 저장
+                        return MatchSchedule.builder()
+                                .matchDate(r.getDate())
+                                .homeTeam(r.getHome())
+                                .awayTeam(r.getAway())
+                                .stadium((r.getPlace() == null || r.getPlace().isBlank()) ? "미정" : r.getPlace())
+                                .build();
+                    });
+                })
+                .toList();
+
+        matchScheduleRepository.saveAll(matchSchedules);
+    }
+
+    // scoreboard에서 경기장(place) 매칭
+    private String findPlace(Map<String, Object> response, Object home, Object away, int year) {
+        List<Map<String, Object>> scoreboard = (List<Map<String, Object>>) response.get("scoreboard");
+
+        return scoreboard.stream()
+                .filter(sb -> {
+                    String sbHomeCode = TeamMapper.changeNameToId((String) sb.get("home"), year);
+                    String sbAwayCode = TeamMapper.changeNameToId((String) sb.get("away"), year);
+                    return home.equals(sbHomeCode) && away.equals(sbAwayCode);
+                })
+                .map(sb -> (String) sb.get("place"))
+                .findFirst()
+                .orElse(null);
     }
 }
