bb0da12932d61d3e7cb1dd3c6de0f6b324b5d7c6
WARDKEY <tjrmsduq@naver.com>
2025-10-14 13:59:47 +0900

    fix(Review): 거래 평가 security 예외 제거

--- src/main/java/basecamp/zikgwan/config/security/JwtAuthenticationFilter.java
+++ src/main/java/basecamp/zikgwan/config/security/JwtAuthenticationFilter.java
@@ -48,8 +48,7 @@ public class JwtAuthenticationFilter extends OncePerRequestFilter {
                 || path.startsWith("/api/sse/")
                 || path.startsWith("/api/socialLogin") //소셜로그인
                 || (path.startsWith("/api/images") && req.getMethod().equals("GET")// 이미지 GET 조회만 인증 제외
-                || path.startsWith("/images/") // 이미지 조회 인증 제외
-                || path.startsWith("/api/review")) // 거래 평가 인증 제외
+                || path.startsWith("/images/")) // 이미지 조회 인증 제외
                 || (path.startsWith("/api/communities") && req.getMethod().equals("GET")) // 모임 GET 조회만 인증 제외
         ) {
             filterChain.doFilter(req, res);
