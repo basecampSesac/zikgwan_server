2237f490971130e0983ca5ba48218a7531ab0893
JikSae <jaesik0174@naver.com>
2025-10-15 20:37:43 +0900

    작업 내용 공유

--- src/main/java/basecamp/zikgwan/ticketsale/controller/TicketSaleController.java
+++ src/main/java/basecamp/zikgwan/ticketsale/controller/TicketSaleController.java
@@ -1,10 +1,13 @@
 package basecamp.zikgwan.ticketsale.controller;
 
+import basecamp.zikgwan.ticketsale.dto.TicketSalePageResponse;
 import basecamp.zikgwan.ticketsale.dto.TicketSaleRequest;
 import basecamp.zikgwan.ticketsale.dto.TicketSaleResponse;
+import basecamp.zikgwan.ticketsale.enums.SortType;
 import basecamp.zikgwan.ticketsale.service.TicketSaleService;
-import java.util.List;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Pageable;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -13,7 +16,10 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RequestPart;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
 
 @RestController
 @RequiredArgsConstructor
@@ -24,22 +30,25 @@ public class TicketSaleController {
 
     // 티켓 판매글 등록
     // TODO security 설정 시 userId 삭제
-    @PostMapping("/{userId}")
+    @PostMapping(value = "/{userId}", consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
     public ResponseEntity<TicketSaleResponse> createTicketSale(
             @PathVariable Long userId,
+            @RequestPart("image") MultipartFile imageFile,
             @RequestBody TicketSaleRequest ticketSaleRequest) {
 
-        TicketSaleResponse ticketSaleResponse = ticketSaleService.createTicketSale(userId, ticketSaleRequest);
+        TicketSaleResponse ticketSaleResponse = ticketSaleService.createTicketSale(userId, ticketSaleRequest,
+                imageFile);
         return ResponseEntity.ok(ticketSaleResponse);
     }
 
     // 티켓 판매글 수정
-    @PutMapping("/{tsId}")
+    @PutMapping(value = "/{tsId}", consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
     public ResponseEntity<TicketSaleResponse> updateTicketSale(
             @PathVariable Long tsId,
+            @RequestPart("image") MultipartFile imageFile,
             @RequestBody TicketSaleRequest ticketSaleRequest) {
 
-        TicketSaleResponse ticketSaleResponse = ticketSaleService.updateTicketSale(tsId, ticketSaleRequest);
+        TicketSaleResponse ticketSaleResponse = ticketSaleService.updateTicketSale(tsId, ticketSaleRequest, imageFile);
         return ResponseEntity.ok(ticketSaleResponse);
     }
 
@@ -53,14 +62,18 @@ public class TicketSaleController {
     // 티켓 판매글 상세 조회
     @GetMapping("/{tsId}")
     public ResponseEntity<TicketSaleResponse> getTicketSaleById(@PathVariable Long tsId) {
-//        TicketSaleResponse dto = ticketSaleService.findTicketSaleById(tsId);
-        return ResponseEntity.ok(ticketSaleService.findTicketSaleById(tsId));
+
+        return ResponseEntity.ok(ticketSaleService.getTicketSaleById(tsId));
     }
 
     // 티켓 판매글 전체 조회
     @GetMapping
-    public ResponseEntity<List<TicketSaleResponse>> getAllTicketSale() {
-        List<TicketSaleResponse> ticketSale = ticketSaleService.findAllTicketSales();
-        return ResponseEntity.ok(ticketSale);
+    public ResponseEntity<TicketSalePageResponse> getAllTicketSales(
+            @RequestParam(required = false, defaultValue = "RECENT") SortType sortType,
+            Pageable pageable
+    ) {
+        TicketSalePageResponse response = ticketSaleService.getAllTicketSales(sortType, pageable);
+
+        return ResponseEntity.ok(response);
     }
 }
--- src/main/java/basecamp/zikgwan/ticketsale/repository/TicketSaleRepository.java
+++ src/main/java/basecamp/zikgwan/ticketsale/repository/TicketSaleRepository.java
@@ -1,11 +1,41 @@
 package basecamp.zikgwan.ticketsale.repository;
 
+import basecamp.zikgwan.common.enums.SaveState;
 import basecamp.zikgwan.ticketsale.TicketSale;
+import java.time.LocalDateTime;
 import java.util.List;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface TicketSaleRepository extends JpaRepository<TicketSale, Long> {
     List<TicketSale> findByTsIdIn(List<Long> tsIds);
+
+    // 최신순
+    Page<TicketSale> findAllBySaveStateOrderByCreatedAtDesc(SaveState saveState, Pageable pageable);
+
+    // 티켓 가격 높은 순
+    Page<TicketSale> findAllBySaveStateOrderByPriceDesc(SaveState saveState, Pageable pageable);
+
+    // 티켓 가격 낮은 순
+    Page<TicketSale> findAllBySaveStateOrderByPriceAsc(SaveState saveState, Pageable pageable);
+
+    // 경기 제목, 모임 구단, 구장, 경기 날짜 선택 조회
+    @Query("SELECT t FROM Ticket_Sales t " +
+            "WHERE (:title IS NULL OR t.title LIKE %:title%) " +
+            "AND (:team IS NULL OR LOWER(t.home) LIKE LOWER(CONCAT('%', :team, '%'))\n " +
+            "OR LOWER(t.away) LIKE LOWER(CONCAT('%', :team, '%'))) " +
+            "AND (:stadium IS NULL OR c.stadium = :stadium) " +
+            "AND (:date IS NULL OR (t.date >= :date AND t.date < :datePlusOne))")
+    List<TicketSale> searchCommunitiesByTitleAndTeamAndStadiumAndDate(
+            @Param("title") String title,
+            @Param("team") String team,
+            @Param("stadium") String stadium,
+            @Param("date") LocalDateTime date,
+            @Param("datePlusOne") LocalDateTime datePlusOne);
+
 }
